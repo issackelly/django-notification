fa42da1d89db9e21590d4a0a3007963358e59d5e
Issac Kelly <issac.kelly@gmail.com>
2011-06-22 11:53:15 -0400

    Fix * import. Add console backend.

--- notification/views.py
+++ notification/views.py
@@ -6,7 +6,7 @@ from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.syndication.views import feed
 
-from notification.models import *
+from notification.models import NoticeType, get_notification_setting, Notice, NOTICE_MEDIA
 from notification.decorators import basic_auth_required, simple_basic_auth_callback
 from notification.feeds import NoticeUserFeed
 
